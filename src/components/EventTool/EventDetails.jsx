466ed451f2bbb67269d30560e556393a078e9d24
Harrison Kerr <hwkerr@clemson.edu>
2023-05-12 15:16:32 -0400

    Add spinner and new event button

--- src/components/EventTool/EventDetails.jsx
+++ src/components/EventTool/EventDetails.jsx
@@ -1,17 +1,24 @@
 import EventForm from "./EventFormFields";
 import Form from "react-bootstrap/Form";
 
-export default function EventDetails({ event }) {
+export default function EventDetails({ event, onCancel }) {
     return (
         <div className="container pt-3 pb-3">
-            <Form id="react-bootstrap-forms-event">
-                <EventForm.Static.Name name={event.name} />
-                <EventForm.Static.Date date={event.date} />
-                <EventForm.Static.Format format={event.format} />
-                <EventForm.Static.Host host={event.host} />
-                <EventForm.Static.Location location={event.location} />
-                <EventForm.Static.Notes notes={event.notes} />
-            </Form>
+            <fieldset disabled>
+                <Form id="react-bootstrap-forms-event">
+                    <EventForm.Static.Name name={event.name} />
+                    <EventForm.Static.Date date={event.date} />
+                    <EventForm.Static.Format format={event.format} />
+                    <EventForm.Static.Host host={event.host} />
+                    <EventForm.Static.Location location={event.location} />
+                    <EventForm.Static.Notes notes={event.notes} />
+                    <div className="d-grid gap-2">
+                        <div className="btn-group">
+                            <Button variant="secondary" size="lg" type="button" onClick={onCancel}>Cancel</Button>
+                        </div>
+                    </div>
+                </Form>
+            </fieldset>
         </div>
     );
 }
--- src/components/EventTool/EventTool.jsx
+++ src/components/EventTool/EventTool.jsx
@@ -1,26 +1,32 @@
 import { useState, useEffect } from 'react';
 import axios from 'axios';
-import Form from 'react-bootstrap/Form';
-import { Row, Col, Button } from 'react-bootstrap';
+import Button from 'react-bootstrap/Button';
 import EventDetails from './EventDetails';
+import { Spinner } from 'react-bootstrap';
 
 const BASE_URL_EVENTS = `https://9v2zi6tk3k.execute-api.us-east-2.amazonaws.com/dev/events`;
 
 export default function EventTool() {
     const [events, setEvents] = useState([]); // stores a list of all event objects
     const [selection, setSelection] = useState(null); // stores an eventId
+    const [loaded, setLoaded] = useState(false);
+
+    const NEW_EVENT = {id: "new", name: "Add New Event Name"};
 
     useEffect(() => {
+        if (loaded) setLoaded(false);
         getFromDatabase("all")
         .then(res => {
             console.log(`Found ${res.data.Count} item(s) in database`);
             setEvents(res.data.Items);
+            setLoaded(true);
         }).catch(err => {
             console.log(err);
+            setLoaded(true);
         });
     }, []);
 
-    const getSelectedEvent = () => events.find(e => e.id = selection);
+    const getSelectedEvent = () => (selection === NEW_EVENT.id ? NEW_EVENT : events.find(e => e.id = selection));
 
     const getFromDatabase = async (id) => {
         const url = BASE_URL_EVENTS + "/" + id;
@@ -53,20 +59,23 @@ export default function EventTool() {
     
     return (
         <div className="EventTool" style={{backgroundColor: "#444444"}} onClick={e => console.log(selection)}>
-            <h3 className="Center">Event Tool</h3>
-            {!selection ?
-                <div className="container scroll">
-                    {events.map((event, i) => getItem(event, i))}
-                </div> :
-                <div className="container scroll">
-                    <EventDetails event={getSelectedEvent()} />
-                    <div className="d-grid gap-2">
-                        <div className="btn-group">
-                            <Button variant="secondary" size="lg" type="button" onClick={clearSelection}>Cancel</Button>
+            <h3 className="center">Event Tool</h3>
+            {!selection ? (
+                loaded ?
+                    <div className="container scroll">
+                        <div className={`listitem-name ${selection === NEW_EVENT.id ? "selected" : ""}`} onClick={() => handleEventClicked(NEW_EVENT)}>
+                            <p>{"+ add event"}</p>
                         </div>
+                        {events.map((event, i) => getItem(event, i))}
+                    </div> :
+                    <div className="container center">
+                        <Spinner animation="border" />
                     </div>
+            ) : (
+                <div className="container scroll">
+                    <EventDetails event={getSelectedEvent()} onCancel={clearSelection} />
                 </div>
-            }
+            )}
         </div>
     );
 }
