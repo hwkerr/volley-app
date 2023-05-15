4c3e5dec1bbd4278633627459cdc6cc356108946
Harrison Kerr <harrisonwkerr@gmail.com>
2023-05-15 16:38:38 -0400

    Add database-connected functions for events

--- src/components/EventTool/EventTool.jsx
+++ src/components/EventTool/EventTool.jsx
@@ -33,9 +33,76 @@ export default function EventTool() {
         return await axios.get(url);
     };
 
+    const saveToDatabase = async (event) => {
+        const url = BASE_URL_EVENTS;
+        return await axios.post(url, event);
+    };
+
+    const deleteFromDatabase = async (id) => {
+        const url = BASE_URL_EVENTS + "/" + id;
+        return await axios.delete(url);
+    }
+
+    const EventKit = {
+        save: async event => {
+            console.log("Save event", event.id, event.name);
+            try {
+                const res = await saveToDatabase(event);
+                console.log(res);
+                addOrUpdateEvent(event); // local
+                setSelection(event.id);
+                return true;
+            } catch (err) {
+                console.error("Error:", err);
+                alert("Failed to save event to database. Check console for logs");
+                return false
+            }
+        },
+        delete: async event => {
+            if (!window.confirm(`Are you sure you would like to delete the event: ${event.name} ${event.name}?`)) {
+                alert("Cancelled delete");
+                return false;
+            };
+            console.log("Remove event", event.id);
+            try {
+                const res = await deleteFromDatabase(event.id); // database
+                console.log(res);
+                removeEvent(event); // local
+                clearSelection();
+                return true;
+            } catch (err) {
+                console.error("Error:", err);
+                alert("Failed to delete event from database. Check console for logs");
+                return false;
+            }
+        }
+    };
+
+    const addOrUpdateEvent = event => {
+        const idToUpdate = event.id;
+        let eventToUpdate = events.find(e => e.id === idToUpdate);
+        if (eventToUpdate === undefined) { // doesn't exist yet -> ADD
+            eventToUpdate = event;
+            setEvents(prev => ([...prev, event]))
+        } else { // aleady exists -> UPDATE
+            for (const property in eventToUpdate) {
+                eventToUpdate[property] = event[property];
+            }
+        }
+        return eventToUpdate;
+    };
+
+    const removeEvent = event => {
+        const originalSize = events.length;
+        const idToRemove = event.id;
+        const newEvents = events.filter(p => p.id !== idToRemove);
+        setEvents(newEvents);
+        return originalSize > newEvents.length; // removed any
+    };
+
     const clearSelection = () => {
         setSelection(null);
-    }
+    };
     
     const handleEventClicked = (event) => {
         if (event.id === selection)
@@ -58,8 +125,8 @@ export default function EventTool() {
     };
     
     return (
-        <div className="EventTool" style={{backgroundColor: "#444444"}} onClick={e => console.log(selection)}>
-            <h3 className="center">Event Tool</h3>
+        <div className="EventTool" style={{backgroundColor: "#444444"}}>
+            <h3 className="center" onClick={e => console.log(selection)}>Event Tool</h3>
             {!selection ? (
                 loaded ?
                     <div className="container scroll">
@@ -73,7 +140,7 @@ export default function EventTool() {
                     </div>
             ) : (
                 <div className="container scroll">
-                    <EventDetails event={getSelectedEvent()} onCancel={clearSelection} />
+                    <EventDetails event={getSelectedEvent()} onCancel={clearSelection} onSave={EventKit.save} />
                 </div>
             )}
         </div>
